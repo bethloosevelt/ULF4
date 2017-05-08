133cacae1c4e8ca3cb02d3906fcafba5db578428
dwesa <wesa.dale@gmail.com>
2017-05-07 20:59:06 -0500

    Block out all board areas

--- scenes/game/view/gameView.lua
+++ scenes/game/view/gameView.lua
@@ -11,8 +11,11 @@ local buttons = require("viewLibs.buttons")
 local controller = require("scenes.game.gameController")
 local viewSpecs = require("scenes.game.view.viewSpecs")
 local boardView = require("scenes.game.view.boardView")
-local boardSceneGroup = display.newGroup()
-local actionBarSceneGroup = display.newGroup()
+local boardDisplayGroup = display.newGroup()
+local actionBarDisplayGroup = display.newGroup()
+local actionBarDisplayGroup = display.newGroup()
+local infoBarDisplayGroup = display.newGroup()
+local animationAreaDisplayGroup = display.newGroup()
 local primarySceneGroup = nil
 local board = nil
 
@@ -25,6 +28,17 @@ local HEIGHT_OF_ACTION_BAR = display.contentHeight - TOP_OF_BOARD - HEIGHT_OF_BO
 local WIDTH_OF_ACTION_BAR = display.contentWidth
 local CENTER_OF_ACTION_BAR_X = display.contentWidth / 2
 local TOP_OF_ACTION_BAR = TOP_OF_BOARD + HEIGHT_OF_BOARD
+
+
+local HEIGHT_OF_INFO_BAR = 100
+local WIDTH_OF_INFO_BAR = display.contentWidth
+local CENTER_OF_INFO_BAR_X = display.contentWidth / 2
+local TOP_OF_INFO_BAR = TOP_OF_BOARD - HEIGHT_OF_INFO_BAR
+
+local HEIGHT_OF_ANIMATION_AREA = TOP_OF_INFO_BAR
+local WIDTH_OF_ANIMATION_AREA = display.contentWidth
+local CENTER_OF_ANIMATION_AREA_x = display.contentWidth / 2
+local TOP_OF_ANIMATION_AREA = 0
 
 
 scene.drawBoard = function(boardModel)
@@ -61,7 +75,7 @@ function setUpTileListeners()
       if event.target.frame <= 26 then
         event.target:setFrame(event.target.frame + 26)
       end
-      boardSceneGroup:dispatchEvent({name= "tileTouched", coordinates = event.target.coordinates})
+      boardDisplayGroup:dispatchEvent({name= "tileTouched", coordinates = event.target.coordinates})
     end
   end
   for rowKey, row in pairs(board) do
@@ -72,25 +86,32 @@ function setUpTileListeners()
 end
 
 scene.createBoard = function(boardModel)
-  board = boardView(boardSceneGroup)
+  board = boardView(boardDisplayGroup)
   setUpTileListeners()
   scene.drawBoard(boardModel)
 end
 
 function initBackgrounds()
-  local boardBackground = display.newRect( boardSceneGroup, CENTER_OF_BOARD_X, display.contentWidth / 2, WIDTH_OF_BOARD, HEIGHT_OF_BOARD )
-  boardBackground:setStrokeColor( 1, 1, 1 )
-  local actionBarBackground = display.newRect( actionBarSceneGroup, CENTER_OF_ACTION_BAR_X, HEIGHT_OF_ACTION_BAR / 2, display.contentWidth, HEIGHT_OF_ACTION_BAR )
-  actionBarBackground:setStrokeColor( 1, .2, .2 )
+  local boardBackground = display.newRect( boardDisplayGroup, CENTER_OF_BOARD_X, display.contentWidth / 2, WIDTH_OF_BOARD, HEIGHT_OF_BOARD )
+  boardBackground:setFillColor( 1, 1, 1 )
+  local actionBarBackground = display.newRect( actionBarDisplayGroup, CENTER_OF_ACTION_BAR_X, HEIGHT_OF_ACTION_BAR / 2, WIDTH_OF_ACTION_BAR, HEIGHT_OF_ACTION_BAR )
+  actionBarBackground:setFillColor( 1, .5, .5 )
+  local infoBarBackground = display.newRect( infoBarDisplayGroup, CENTER_OF_INFO_BAR_X, HEIGHT_OF_INFO_BAR / 2, WIDTH_OF_INFO_BAR, HEIGHT_OF_INFO_BAR)
+  infoBarBackground:setFillColor(.5, .5, 1)
+  local animationAreaBackground = display.newRect( animationAreaDisplayGroup, CENTER_OF_ANIMATION_AREA_x, HEIGHT_OF_ANIMATION_AREA / 2, WIDTH_OF_ANIMATION_AREA, HEIGHT_OF_ANIMATION_AREA )
+  animationAreaBackground:setFillColor(.5, 1, .5)
 end
 
 function scene:create( event )
   composer.removeHidden()
   primarySceneGroup = self.view
-  primarySceneGroup:insert(boardSceneGroup)
-  primarySceneGroup:insert(actionBarSceneGroup)
-  boardSceneGroup.y = TOP_OF_BOARD
-  actionBarSceneGroup.y = TOP_OF_ACTION_BAR
+  primarySceneGroup:insert(boardDisplayGroup)
+  primarySceneGroup:insert(infoBarDisplayGroup)
+  primarySceneGroup:insert(actionBarDisplayGroup)
+  primarySceneGroup:insert(animationAreaDisplayGroup)
+  boardDisplayGroup.y = TOP_OF_BOARD
+  actionBarDisplayGroup.y = TOP_OF_ACTION_BAR
+  infoBarDisplayGroup.y = TOP_OF_INFO_BAR
   initBackgrounds()
   controller.controlScene(scene, event.params.difficulty, event.params.characters)
 end
@@ -98,7 +119,7 @@ end
 function scene:destroy( event )
 end
 
-boardSceneGroup:addEventListener( "tileTouched", tileTouched)
+boardDisplayGroup:addEventListener( "tileTouched", tileTouched)
 scene:addEventListener( "create", scene )
 scene:addEventListener( "destroy", scene )
 scene:addEventListener( "update", scene )
